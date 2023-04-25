b820012b12d7c43f49a1191e541ba7b13c3b292f
Tudyx <56633664+Tudyx@users.noreply.github.com>
2023-04-25 19:35:38 +0200

    feat: plot benchmark result

--- src/plot_res.rs
+++ src/plot_res.rs
@@ -2,6 +2,8 @@ use std::{error::Error, path::PathBuf};
 
 use polars::prelude::*;
 
+use plotly::{common::Title, layout::Axis, Bar, Layout, Plot};
+
 use clap::Parser;
 
 #[derive(Parser)]
@@ -12,9 +14,9 @@ struct Cli {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let cli = Cli::parse();
+    let mut cli = Cli::parse();
 
-    let df = CsvReader::from_path(cli.csv_path)?
+    let df = CsvReader::from_path(&cli.csv_path)?
         .has_header(true)
         .finish()?;
 
@@ -49,6 +51,49 @@ fn main() -> Result<(), Box<dyn Error>> {
         .collect()?;
 
     println!("{}", df);
+
+    let x = df
+        .column("batch_size")
+        .unwrap()
+        .iter()
+        .map(|x| x.try_extract::<u32>().unwrap())
+        .collect::<Vec<_>>();
+
+    let pytorch_speed = df
+        .filter(&df.column("library")?.equal("pytorch")?)?
+        .column("sample_per_seconds")
+        .unwrap()
+        .iter()
+        .map(|y| y.try_extract::<u32>().unwrap())
+        .collect::<Vec<_>>();
+
+    let ai_dataloader_speed = df
+        .filter(&df.column("library")?.equal("ai-dataloader")?)?
+        .column("sample_per_seconds")
+        .unwrap()
+        .iter()
+        .map(|y| y.try_extract::<u32>().unwrap())
+        .collect::<Vec<_>>();
+
+    let pytorch_trace = Bar::new(x.clone(), pytorch_speed).name("PyTorch");
+    let ai_dataloader_trace = Bar::new(x, ai_dataloader_speed).name("ai-dataloader");
+
+    let layout = Layout::new()
+        .title(Title::new(
+            "Speed comparison between Pytorch and ai-dataloader",
+        ))
+        .x_axis(Axis::new().title(Title::new("batch size")))
+        .y_axis(Axis::new().title(Title::new("speed (image/s)")));
+
+    let mut plot = Plot::new();
+    plot.set_layout(layout);
+    plot.add_trace(pytorch_trace);
+    plot.add_trace(ai_dataloader_trace);
+    plot.show();
+    println!("{}", plot.to_inline_html(Some("basic_bar_chart")));
+    cli.csv_path.set_extension("html");
+
+    plot.write_html(cli.csv_path);
 
     Ok(())
 }
